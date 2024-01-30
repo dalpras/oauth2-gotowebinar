381aba1595f5fc382e6f845d3cf5492bc007de4d
Stefano Dal Prà <Stefano Dal Pr>
2024-01-30 15:30:25 +0000

    change datatype DateTimeImmutable

--- src/Helper/DateUtcHelper.php
+++ src/Helper/DateUtcHelper.php
@@ -1,26 +1,27 @@
-<?php
+<?php declare(strict_types=1);
+
 namespace DalPraS\OAuth2\Client\Helper;
+
+use DateTime;
+use DateTimeImmutable;
+use DateTimeZone;
 
 class DateUtcHelper 
 {
     /**
      * Convert DateTime in UTC timezone string format Y-m-d\TH:i:s\Z
-     *
-     * @param \DateTime $dateTime Local datetime
-     * @return string
      */
-    public static function date2utc(\DateTimeInterface $dateTime) : string {
-        return $dateTime->setTimezone(new \DateTimeZone('UTC'))->format('Y-m-d\TH:i:s\Z');
+    public static function date2utc(DateTime|DateTimeImmutable $dateTime): string 
+    {
+        return $dateTime->setTimezone(new DateTimeZone('UTC'))->format('Y-m-d\TH:i:s\Z');
     }
 
     /**
      * Convert string UTC datetime in current datetime
-     * 
-     * @param string $utcTime String representing the time
-     * @return \DateTime
      */
-    public static function utc2date(string $utcTime) : \DateTime {
-        return (new \DateTime())->createFromFormat('Y-m-d\TH:i:s\Z', $utcTime);
+    public static function utc2date(string $utcTime): DateTime 
+    {
+        return (new DateTime())->createFromFormat('Y-m-d\TH:i:s\Z', $utcTime);
     }
 }
 
